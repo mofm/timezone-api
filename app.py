4f9377532f2eb9e061430ae55524486f5d26053e
mofm <emre.eryilmaz@piesso.com>
2021-05-11 01:54:53 +0300

    timezone api initial commit

---
+++ app.py
@@ -0,0 +1,60 @@
+"""Lightweight Timezone API with Flask and TimezoneFinderL"""
+import os
+import logging
+from time import time
+import flask
+from flask import Flask, request, jsonify
+from timezonefinder import TimezoneFinderL
+
+
+# Version of this APP template
+__version__ = '0.0.1'
+# Read env variables
+SERVICE_START_TIMESTAMP = time()
+DEBUG = os.environ.get('DEBUG', False)
+
+
+app = Flask(__name__)
+# Gunicorn log level as Flask's log level
+if __name__ != '__main__':
+    gunicorn_logger = logging.getLogger('gunicorn.error')
+    app.logger.handlers = gunicorn_logger.handlers
+    app.logger.setLevel(gunicorn_logger.level)
+
+
+@app.route('/timezone/api/', methods=['GET'])
+def get_timezone():
+    """check values and search timezones."""
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    tifi = TimezoneFinderL(in_memory=True)
+    if (lat and lng is not None) and (len(lat) and len(lng) != 0):
+        pass
+    else:
+        return jsonify(message="No latitude or longitude values", status=400)
+    try:
+        lng, lat = float(lng), float(lat)
+        return jsonify({'tz_name': tifi.timezone_at(lng=lng, lat=lat), 'status': 200})
+    except ValueError:
+        return jsonify(message="lat lng out of bounds", status=422)
+
+
+@app.route('/timezone/health/', methods=['GET'])
+def health_check():
+    """api status"""
+    return flask.Response("OK", status=200)
+
+
+@app.route('/timezone/info/', methods=['GET'])
+def api_info():
+    """api infos"""
+    info = {
+        'version':  __version__,
+        'running-since': SERVICE_START_TIMESTAMP,
+        'debug': DEBUG
+    }
+    return jsonify(info)
+
+
+if __name__ == '__main__':
+    app.run()
