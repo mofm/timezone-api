0691842f4a8658c226b82a4419d42414d38d148d
mofm <emre.eryilmaz@piesso.com>
2021-05-14 02:36:36 +0300

    added dst timezone

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 """Lightweight Timezone API with Flask and TimezoneFinderL"""
-from flask import Flask, jsonify
-from flask_restful import Resource, Api, reqparse
+import os
+import logging
+from time import time
+from datetime import datetime
+import pytz
+import flask
+from flask import Flask, request, jsonify
 from timezonefinder import TimezoneFinderL
 
 app = Flask(__name__)
@@ -10,15 +15,28 @@ parser = reqparse.RequestParser()
 parser.add_argument('lat', type=float, required=True, location='args')
 parser.add_argument('lng', type=float, required=True, location='args')
 
-class timezone(Resource):
-    def get(self):
-        args = parser.parse_args(strict=True)
-        tifi = TimezoneFinderL(in_memory=True)
+@app.route('/timezone/api/', methods=['GET'])
+def get_timezone():
+    """check values and search timezones."""
+    lat = request.args.get('lat')
+    lng = request.args.get('lng')
+    tstamp = request.args.get('timestamp')
+    tfind = TimezoneFinderL(in_memory=True)
+    if (None not in {lat, lng, tstamp}) and (0 not in {len(lat), len(lng), len(tstamp)}):
+        pass
+    else:
+        return jsonify(message="missing required argument(s)", status=400)
+    try:
+        lng, lat = float(lng), float(lat)
+        tstamp = int(tstamp)
+        tzone = tfind.timezone_at(lng=lng, lat=lat)
+        tz = pytz.timezone(tzone)
+        tz_time = datetime.fromtimestamp(tstamp)
+        dst_offset = tz.dst(tz_time, is_dst=False).total_seconds()
+        return jsonify({'dstOffset': dst_offset, 'tzname': tzone, 'status': 200})
 
-        try:
-            return jsonify({'tz_name': tifi.timezone_at(lng=args['lng'], lat=args['lat']), 'status': 200})
-        except ValueError:
-            return jsonify(message="lat lng out of bounds", status=422)
+    except ValueError:
+        return jsonify(message="lat lng out of bounds", status=422)
 
 
 api.add_resource(timezone, '/')
